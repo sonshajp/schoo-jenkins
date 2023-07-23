0808031d03bd7e90b4b957b96ef3436af8110ffe
FireOfFate <131586788+sonshajp@users.noreply.github.com>
2023-07-23 19:46:50 +0900

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 cd "%~pd0"
 mkdir target\classes
+java -version
 javac -cp "lib\*" -d "target\classes" src/main/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/runner/*.java
